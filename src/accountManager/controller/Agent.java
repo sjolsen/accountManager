1a5cdc9c654c9ea4337bf0dc5806cbe5db062397
Stuart Olsen <so1132@txstate.edu>
2014-04-27 22:31:19 -0500

    Extracted Agent superclass

---
+++ src/accountManager/controller/Agent.java
@@ -0,0 +1,57 @@
+package accountManager.controller;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import accountManager.model.account.Account;
+import accountManager.model.money.Money;
+
+public class Agent implements Observer {
+
+	protected final Controller controller;
+	protected final int id;
+	protected final Money step;
+	protected final Account account;
+	protected int operations_performed;
+	protected Money amount_transferred;
+	protected volatile State state;
+
+	public enum State
+	{
+		running,
+		blocked, 
+		stopped
+	}
+
+	public Agent (Controller controller, int id, Money step, Account account)
+	{
+		this.controller = controller;
+		this.id = id;
+		this.step = step;
+		this.account = account;
+		account.addObserver (this);
+		
+		operations_performed = 0;
+		amount_transferred = new Money (0);
+		state = State.stopped;
+	}
+
+	protected void setState(State state) {
+		this.state = state;
+		System.err.format ("Agent %d entering state %s\n", id, state.name ());
+	}
+
+	public void stop() {
+		setState (State.stopped);
+	}
+
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		if (state == State.blocked)
+		{
+			setState (State.running);
+			this.notifyAll ();
+		}
+	}
+
+}
--- src/accountManager/controller/WithdrawAgent.java
+++ src/accountManager/controller/WithdrawAgent.java
@@ -1,46 +1,16 @@
 package accountManager.controller;
 
-import java.util.Observable;
 import java.util.Observer;
 
 import accountManager.model.account.Account;
 import accountManager.model.account.AccountUnderflowException;
 import accountManager.model.money.Money;
 
-public class WithdrawAgent implements Observer
+public class WithdrawAgent extends Agent implements Observer
 {
-	final Controller controller;
-	final int id;
-	final Money step;
-	final Account account;
-	int operations_performed;
-	Money amount_transferred;
-	volatile State state;
-	
-	public enum State
-	{
-		running,
-		blocked, 
-		stopped
-	}
-	
-	private void setState (State state)
-	{
-		this.state = state;
-		System.err.format ("Agent %d entering state %s\n", id, state.name ());
-	}
-	
 	public WithdrawAgent (Controller controller, int id, Money step, Account account)
 	{
-		this.controller = controller;
-		this.id = id;
-		this.step = step;
-		this.account = account;
-		account.addObserver (this);
-		
-		operations_performed = 0;
-		amount_transferred = new Money (0);
-		state = State.stopped;
+		super (controller, id, step, account);
 	}
 	
 	public void run () throws AccountUnderflowException
@@ -79,21 +49,6 @@ public class WithdrawAgent implements Observer
 					}
 				}
 			}
-		}
-	}
-	
-	public void stop ()
-	{
-		setState (State.stopped);
-	}
-
-	@Override
-	public void update(Observable arg0, Object arg1)
-	{
-		if (state == State.blocked)
-		{
-			setState (State.running);
-			this.notifyAll ();
 		}
 	}
 }
